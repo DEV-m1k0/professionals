ae038b1dfd8b619be293d12fd4209aaf39828fd0
Anton Knyazev <knyazevmac@icloud.com>
2025-02-09 17:22:05 +0300

    разработка окна с новостями V1

--- mobile_app/src/main.py
+++ mobile_app/src/main.py
@@ -1,8 +1,29 @@
 import flet as ft
+from components import get_container_with_navigate_btns
+from news import get_block_news
+
+
 
 
 def main(page: ft.Page):
-    pass
+    page.adaptive = True
+    page.bgcolor = ft.colors.GREEN_900
+
+    body = ft.Container(
+        content=ft.Column(
+            controls=[
+                get_container_with_navigate_btns(),
+                get_block_news(page=page)
+            ]
+        )
+    )
+    
+    
+
+    page.add(
+        body
+    )
 
 
 ft.app(main)
+
